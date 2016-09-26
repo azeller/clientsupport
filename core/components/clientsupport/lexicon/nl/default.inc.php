d873e9ff9dfd04343fbb37e874e96b36ced69825
Joeke Kloosterman <joeke@sterc.nl>
2016-09-26 09:17:21 +0200

    Add NL lexicon + update docs + add new transport zip

---
+++ core/components/clientsupport/lexicon/nl/default.inc.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Default Dutch Lexicon Entries for ClientSupport
+ *
+ * @package clientsupport
+ * @subpackage lexicon
+ */
+
+$_lang['clientsupport'] = 'ClientSupport';
+$_lang['clientsupport.help'] = 'Help';
+$_lang['clientsupport.menu'] = 'Help';
+$_lang['clientsupport.menu_desc'] = 'Help';
+$_lang['clientsupport.header'] = 'Kunnen we u ergens mee helpen?';
+$_lang['clientsupport.help.description'] = 'Vul onderstaand formulier in en onze service afdeling gaat er direct mee aan de slag.';
+$_lang['clientsupport.name'] = 'Uw naam';
+$_lang['clientsupport.email'] = 'Uw e-mailadres';
+$_lang['clientsupport.problem'] = 'Probleem';
+$_lang['clientsupport.problem.label'] = 'Omschrijf in 1 korte zin wat het probleem is.';
+$_lang['clientsupport.problem.message'] = 'Omschrijving (optioneel)';
+$_lang['clientsupport.notice'] = 'Naast bovenstaande gegevens worden ook uw browser versie, pagina URL, gebruikersnaam en IP adres naar ons verstuurd.';
+$_lang['clientsupport.form.cancel'] = 'Annuleren';
+$_lang['clientsupport.form.submit'] = 'Versturen';
+$_lang['clientsupport.browser'] = 'Browser';
+$_lang['clientsupport.platform'] = 'Platform';
+$_lang['clientsupport.url'] = 'Pagina URL';
+$_lang['clientsupport.username'] = 'MODX gebruikersnaam';
+$_lang['clientsupport.ip_address'] = 'Gebruiker IP address';
+$_lang['clientsupport.email.subject'] = 'Clientsupport: [[+subject]]';
+$_lang['clientsupport.message_sent'] = 'Uw bericht is succesvol verstuurd.';
+
+//Settings
+$_lang['setting_clientsupport.email_to'] = 'Email-to address';
+$_lang['setting_clientsupport.email_to_desc'] = 'The emailaddress where the support form is sent to. Defaults to system setting "emailsender".';
+$_lang['setting_clientsupport.email_from'] = 'Email-from address';
+$_lang['setting_clientsupport.email_from_desc'] = 'The emailaddress where the support email is sent from. Defaults to system setting "emailsender".';
+$_lang['setting_clientsupport.email_from_name'] = 'Email-from name';
+$_lang['setting_clientsupport.email_from_name_desc'] = 'Use this to override the name of the email-sender where the support form is sent to. Defaults to name supplied in the form.';
+$_lang['setting_clientsupport.email_tpl'] = 'Email tpl';
+$_lang['setting_clientsupport.email_tpl_desc'] = 'The chunk that is used for the support email.';
